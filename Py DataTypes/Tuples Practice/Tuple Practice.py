1529393a8f4fee158462aaa1ee6533d03af030c1
RobStepanyan <robert1stepanyan@gmail.com>
2019-01-08 08:50:27 +0400

    Updated Tuples.py

--- Py DataTypes/Tuples Practice/Tuple Practice.py
+++ Py DataTypes/Tuples Practice/Tuple Practice.py
@@ -17,3 +17,44 @@ n1, n2, n3 = tp
 print(n1)
 print(n2)
 print(n3)
+
+# 4.
+# Add an item in a tuple with different ways.
+tuplex = (1, 2, 3, 4)
+print(tuplex)
+
+tuplex = tuplex + (5,)
+print(tuplex)
+
+tuplex = list(tuplex)
+tuplex.append(6)
+print(tuple(tuplex))
+tuplex.remove(6)
+tuplex = tuple(tuplex)
+print(tuplex)
+
+tuplex = tuplex[:4] + (6,) + tuple(reversed(tuplex[:4]))
+print(tuplex)
+
+# 5.
+# Tuple to a string
+t = ("H", "o", "w", "d", "y", "h", "o", ", ", "b", "ch.")
+s = "".join(t)
+print(s)
+
+# 6.
+# Get the 4th element and 4th element from last of a tuple.
+t = (1, 2, 3, "4", 5, 6, "7", 8, 9 ,10)
+print("4th element:", t[3])
+print("4th element from last:", t[-4])
+
+# 7.
+# Create the colon of a tuple.
+from copy import deepcopy
+t = (1, 2, [])
+print(t)
+tt = deepcopy(t)
+tt[2].append("Hi")
+print(tt)
+print(t)
+
