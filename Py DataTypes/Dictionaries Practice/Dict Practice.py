52785cfa46d23dd9a2e376e7c1b38a89b5f060a2
Robert <robert1stepanyan@gmail.com>
2019-01-13 11:54:01 +0400

    Updated Dict Practice.py

--- Py DataTypes/Dictionaries Practice/Dict Practice.py
+++ Py DataTypes/Dictionaries Practice/Dict Practice.py
@@ -39,4 +39,64 @@ def itd(d):
 itd(d)
 
 # 5.
+# Write a Python program to sum all the items in a dictionary.
+d = {1: 10, 2: 20, 3:30}
+print(sum(d.values()))
+
+# 6.
+# Write a Python program to multiply all the items in a dictionary.
+def md(d):
+    prd = 1
+    for x in d.values():
+        prd *= x
+    return prd
+
+print(md({1: 10, 2: 20, 3:30}))
+
+# 7.
+# Write a Python script to generate and print a dictionary
+# that contains a number (between 1 and n) in the form (x, x*x).
+def cd(n):
+    d = {}
+    for i in range(1,n+1):
+        d[i]= i**2
+    return d
+
+n = int(input("Input n: "))
+print(cd(n))
+
+# 8.
+# Write a Python script to print a dictionary where the keys are numbers between
+# 1 and 15 (both included) and the values are square of keys.
+d = {i: i*i for i in range(1, 16)}
+print(d)
+
+# 9.
+# Write a Python script to merge two Python dictionaries.
+d1 = {1: 10, 2:20}
+d2 = {3: 30, 4: 40}
+d = d1.copy()
+d.update(d2)
+print(d)
+
+# 10.
+# Write a Python program to remove a key from a dictionary.
+d = {1: 10, 2: 20, 3: 30, 4: 40}
+print("D: ",d)
+k = int(input("What key to remove? "))
+if k in d:
+    del d[k]
+    print("Key was successfuly deleted!")
+    print("D: ",d)
+else:
+    print("Key do not exist!")
+
+# 11.
+# Write a Python program to map two lists into a dictionary.
+k = ['red', 'green', 'blue']
+v = ['#FF0000','#008000', '#0000FF']
+d = dict(zip(k, v))
+print(d)
+
+# 12.
 #
