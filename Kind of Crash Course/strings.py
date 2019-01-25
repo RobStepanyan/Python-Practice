845cf5919359a3df1f3c9127729991a9026c5eac
RobStepanyan <robert1stepanyan@gmail.com>
2019-01-25 13:43:27 +0400

    Create Crash Course/

---
+++ Kind of Crash Course/strings.py
@@ -0,0 +1,15 @@
+# Python built-in class "Str" which stands for String are used 
+# for working with textual data.
+# Python strings are "immutable" which means they cannot be 
+# changed after they are created. Since strings can't be changed,
+# we construct *new* strings as we go to represent computed values. 
+# So for example the expression ('hello' + 'there') takes in the 
+# 2 strings 'hello' and 'there' and builds a new string 'hellothere'.
+
+# Same to Armenian
+# Python-ի ներդրված կլաս "Str"-ը որը նույնն է ինչ String, օգտագործվում
+# է տեքստային տվյալների հետ աշխատելու համար:
+# Python-ի string-երը անփոփոխելի են, ինչը նշանակում է, որ նրանք չեն 
+# կարող փոփոխել իրենց արժեքը ստանալուց հետո:
+# Օրինակ, մեկ ունենք այսպիսի արտահայտություն ('Բարև' + 'բոլորին')
+# Python-ը կվերցնի այդ երկուսը և կստեղծի մի նոր String` "Բարևբոլորին"
