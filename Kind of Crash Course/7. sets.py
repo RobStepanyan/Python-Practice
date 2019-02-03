8e7a85e83e21592045111c08b9cf14abbaaf625a
RobStepanyan <robert1stepanyan@gmail.com>
2019-02-03 11:02:10 +0400

    Created sets

--- Kind of Crash Course/7. sets.py
+++ Kind of Crash Course/7. sets.py
@@ -1,1 +1,13 @@
-# Under Construction
+"""
+Sets
+A set is an unordered “bag” of unique values. A single set can contain values of any immutable datatype. 
+Once you have two sets, you can do standard set operations like union, intersection, and set difference. 
+
+Set-եր
+Set-ը դա չդասավորված արժեքների պարկ է: Set-ը կարող է պարունակել ցանկացած immutable(չփոփոխվող)
+արժեք: Ունենալով երկու set-եր դուք կարող եք կատարել set-ի հիմնական գործողությունները, օրինակ`
+union - երկուսի տարրերը միասին,
+intersection - երկուսի համար ընդհանուր տարրեր (հատում)
+diiference - տարրեր որոնք կան միայն մեկի մոտ
+symmetric difference - տարրեր որոնք չունեն իրենց կրկնօրինակից մյուսը set-ում, և հակառակը
+(union-ի, intersection-ի....մասին կա ավելի պարզ նկարագրող նկար())
